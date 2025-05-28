6cdb4290c9b5e881fb8f3a3a40013d6783b7e554
kmgyu <lidersy961@gmail.com>
2025-05-28 11:40:02 +0900

    fix: 병합오류 수정

--- front-end/.eslintrc.js
+++ front-end/.eslintrc.js
@@ -26,31 +26,4 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
-}module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    },
-    env: {
-        browser: true,
-        node: true, // ✅ 이 줄을 추가하면 process 허용됨
-        es2021: true,
-    },
-    globals: {
-        defineProps: 'readonly',
-        defineEmits: 'readonly',
-    }
-};
+}
