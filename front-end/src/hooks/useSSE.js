ed35d0e120341eb4b831548c7847861de0408265
kmgyu <lidersy961@gmail.com>
2025-06-02 09:41:52 +0900

    [comment] 주석화된 레거시 코드 삭제

--- front-end/src/hooks/useSSE.js
+++ front-end/src/hooks/useSSE.js
@@ -1,30 +1,6 @@
 import { onMounted, onUnmounted } from 'vue';
 
 export function useSSE(url, onMessage) {
-  // let intervalId = null;
-
-  // // 더미 데이터 생성 함수 (원하는 구조에 맞게 조정 가능)
-  // const generateMockData = () => {
-  //   return {
-  //     timestamp: new Date().toISOString(),
-  //     menu_id: 1,
-  //     stock_quantity: Math.floor(Math.random() * 10),
-  //   };
-  // };
-
-  // onMounted(() => {
-  //   // 3초마다 가짜 데이터 전송
-  //   intervalId = setInterval(() => {
-  //     const mockData = generateMockData();
-  //     onMessage(mockData);
-  //   }, 3000);
-  // });
-
-  // onUnmounted(() => {
-  //   if (intervalId) {
-  //     clearInterval(intervalId);
-  //   }
-  // });
   let eventSource = null;
 
   onMounted(() => {
